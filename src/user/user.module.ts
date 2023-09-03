ba12ecf600695b5d12cbea617cecd4d75bdacbb1
Mahmoud Wardany <106314476+mahmoudwardany@users.noreply.github.com>
2023-09-03 04:34:23 +0300

    Update user.module.ts

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -16,6 +16,7 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
       imports: [ConfigModule],
       useFactory: async (configService: ConfigService) => ({
         secret: configService.get<string>('JWT_SECRET'),
+         signOptions: { expiresIn: '1d' },
       }),
       inject: [ConfigService],
     }),
